34950952c1bec730a1590c7946f36d5122615de1
walker <100868154+wa1ker38552@users.noreply.github.com>
2023-05-31 11:36:07 -0700

    Update ChessAI.java

--- ChessAI.java
+++ ChessAI.java
@@ -1,10 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.*;
 
 public class ChessAI {
 	private ChessEngine engine;
 	private HashMap<String, Integer> pointValues = new HashMap<String, Integer>();
+	private NotationCalculator notationCalculator;
 	
 	public ChessAI(ChessEngine engine) {
+		this.notationCalculator = new NotationCalculator(engine);
 		this.engine = engine;
 		this.pointValues.put(null, 0);
 		this.pointValues.put("WP", 10);
@@ -34,6 +43,25 @@ public class ChessAI {
 	}
 	
 	public Move getBestMove() {
+		if (this.engine.getFromStockfish()) {
+			Stockfish stockfish = new Stockfish("assets\\\\stockfish-windows-2022-x86-64-avx2.exe");
+			
+			ChessTile[][] board = this.engine.getGameState();
+			String notation = notationCalculator.getFEN(board).replace(" ", "%20");
+			String move = stockfish.getBestMove(notation, 10);
+			int startCol = "abcdefgh".indexOf(move.substring(0, 1));
+			int startRow = 7-(Integer.parseInt(move.substring(1, 2))-1);
+			int endCol = "abcdefgh".indexOf(move.substring(2, 3));
+			int endRow = 7-(Integer.parseInt(move.substring(3, 4))-1);
+			// System.out.println(startCol+", "+startRow+" "+endCol+", "+endRow);
+			// System.out.println(move);
+			
+			return new Move(board[startRow][startCol], board[endRow][endCol]);
+		}
+		return calculateSelfBestMove(false);
+	}
+	
+	public Move calculateSelfBestMove(boolean debug) {
 		HashMap<Move, Integer> validMoves = new HashMap<Move, Integer>();
 		ArrayList<ChessTile> selfPieces = new ArrayList<ChessTile>();
 		ArrayList<ChessTile> possibleTargets = new ArrayList<ChessTile>();
@@ -62,24 +90,34 @@ public class ChessAI {
 				}
 			}
 		}
-		
+
+		int average = 0;
 		for (Move move : validMoves.keySet()) {
-			for (ChessTile validTile : possibleTargets) {
-				if (validTile.hasPiece()) {
-					// Iterate over valid tiles with white pieces on them in the presepctive of white
-					Pos position = move.getTarget().getPosition().calculateDistance(validTile.getPosition());
-					// Fake tile
-					ChessTile fakeTile = new ChessTile(move.getTarget().getPosition(), engine);
-					fakeTile.setPiece(move.getSelf().getPiece());
-					if (engine.checkValidMove(position, validTile, fakeTile)) {
-						int pointValue = validMoves.get(move);
-						pointValue -= this.pointValues.get(move.getSelf().getPiece().getPiece());
-						validMoves.replace(move, pointValue);
+			average += validMoves.get(move);
+		}
+		
+		if (average == 0) {
+			if (debug) {System.out.print("Recalculated: ->");}
+			for (Move move : validMoves.keySet()) {
+				for (ChessTile validTile : possibleTargets) {
+					if (validTile.hasPiece()) {
+						// Iterate over valid tiles with white pieces on them in the presepctive of white
+						Pos position = move.getTarget().getPosition().calculateDistance(validTile.getPosition());
+						// Fake tile
+						ChessTile fakeTile = new ChessTile(move.getTarget().getPosition(), engine);
+						fakeTile.setPiece(move.getSelf().getPiece());
+						if (engine.checkValidMove(position, validTile, fakeTile)) {
+							validMoves.replace(move, -this.pointValues.get(move.getSelf().getPiece().getPiece()));
+						}
 					}
 				}
 			}
 		}
 		
+		if (debug) {
+			System.out.println(validMoves);
+		}
+	
 		return getHighestScore(validMoves);
 	}
 }
